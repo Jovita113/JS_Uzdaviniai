0ae2454c404c7020d02da6a73d22f221523a59a8
Jovita113 <99712952+Jovita113@users.noreply.github.com>
2022-04-06 19:20:14 +0300

    Create JS_salygosUzdaviniai.js

---
+++ JS_salygosUzdaviniai.js
@@ -0,0 +1,124 @@
+"use strict";
+
+// pirmo uzdavinio sprendimas
+//
+// var n = 0;
+// var answer = n;
+//
+// if (answer > 0) {
+//    console.log("Skaičius geras");
+// } else {
+//    console.log("Skaičius blogas");
+// }
+
+
+// antro uzdavinio sprendimas
+// 
+// var n = 2;
+// var answer = n;
+//
+// if (answer >= 2) {
+//    console.log("Palaukite");
+// } else {
+//    console.log("Eikite");
+// }
+
+
+// trecio uzdavinio sprendimas 
+//   
+// let d = 3;
+// let k = 18;
+// let n = 5;
+// let answer = d * n;
+// if (answer >= k ) {
+//    console.log("Knygos telpa į dežes");
+// } else {
+//    console.log("Knygos netelpa į dežes");
+// } 
+
+
+// ketvirto uždavinio sprendimas
+//
+// let a = 69;
+// let b = 6;
+// let c = a - b;
+// if (c > 1) {
+//    console.log (a - 1);
+// } else {
+//   console.log (b + 1);
+// }
+
+
+//penkto uzdavinio sprendimas 
+/*
+   let c = 50;
+   let p = 20;
+   let m = c % p;
+   let k = c / p;
+    console.log(Math.floor(k));
+    console.log(m);
+*/
+   
+
+//sesto uzdavinio sprendimas
+/*
+ let n = 4;
+ let c = n % 3;
+ if (c < 1) {                                                                                                                                   
+    console.log("Trikampi sudėlioti galima");
+ } else {
+    console.log("Trikampio sudėlioti negalima");
+ }
+*/
+
+
+//septinto uzdavinio sprendimas
+/*
+let n = 7;
+let z = 12 / n ;
+if (n == 2) {
+    console.log("28");
+} else if (z > 1) {
+    console.log("31");
+} else {
+    console.log("30");
+}
+*/
+
+
+//astunto uzdavinio sprendimas
+/*
+let k = 1;
+if (k==1 && k==3) {
+    console.log ("Kambarį tvarkys jaunėlis");
+} else  {
+    console.log ("Kambarį tvarkys vyresnėlis")
+}
+*/
+
+
+//devinto uzdavinio sprendimas
+/*
+let m = 2005;
+let n = ((m - 1896)/4)+1;
+if (n%1==0) {
+    console.log ("n=" + n);
+} else {
+    console.log ("Metai neolimpiniai");
+}*/
+
+
+//desimto uzdavinio sprendimas
+/*
+let v1 = 8;
+let m1 = 29;
+let m2 = 23;
+let v = 9;
+let m = 5;
+let answer = m2-((v * 60 + m) - (v1 * 60 + m1));
+if (answer >= 0) {
+    console.log("Petras į pamoką pavėluos");
+} else {
+    console.log("Petras į pamoką nepavėluos");
+}
+*/
